4ccea2e5df617c66fdc25c96e91b2ad04fdc5cca
jublx <juju.clipet@gmail.com>
2020-12-28 18:43:13 +0100

    Ajout du panneau d'affichage des réseaux sociaux

--- public/js/app.js
+++ public/js/app.js
@@ -2306,8 +2306,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   store: _store_js__WEBPACK_IMPORTED_MODULE_0__["default"],
@@ -2423,6 +2421,63 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _store_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../store.js */ "./resources/js/store.js");
 /* harmony import */ var _ContactsBrowser_vue__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ContactsBrowser.vue */ "./resources/js/components/ContactsBrowser.vue");
 /* harmony import */ var _ModificationFormulary_vue__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ModificationFormulary.vue */ "./resources/js/components/ModificationFormulary.vue");
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -7118,7 +7173,7 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 
 // module
-exports.push([module.i, "\n.btn-custom[data-v-84a34bd4] {\n  float: right;\n  margin-bottom: 6px;\n}\n", ""]);
+exports.push([module.i, "\n.btn-custom[data-v-84a34bd4] {\n  float: right;\n  margin-bottom: 6px;\n}\n.custom-label[data-v-84a34bd4] {\n  font-size: 1.2em;\n}\n.form-control[data-v-84a34bd4] {\n  font-size: 1.1em;\n  color: #008383;\n}\n", ""]);
 
 // exports
 
@@ -7156,7 +7211,7 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 
 // module
-exports.push([module.i, "\nh1[data-v-21d2b2bd] {\n  margin: 10px;\n}\nh4[data-v-21d2b2bd], p[data-v-21d2b2bd] {\n  text-align: center;\n}\np[data-v-21d2b2bd] {\n  font-size: 1.1em;\n}\n.page[data-v-21d2b2bd] {\n  margin-top: 100px;\n}\n.bg-custom[data-v-21d2b2bd] {\n  background-image: linear-gradient(to right bottom, #4eb888, #37ab8a, #219e8a, #0b9187, #008383);\n  color: #f8fafc;\n}\n.card[data-v-21d2b2bd] {\n  font-size: 1.2em;\n}\n.btn-modal[data-v-21d2b2bd] {\n  margin-bottom: -30px;\n}\n.text-muted[data-v-21d2b2bd] {\n  margin: 0;\n}\n.form-control[data-v-21d2b2bd] {\n  width: 22%;\n  text-align: center;\n}\n\n", ""]);
+exports.push([module.i, "\nh1[data-v-21d2b2bd] {\n  margin: 10px;\n}\nh4[data-v-21d2b2bd], p[data-v-21d2b2bd] {\n  text-align: center;\n}\np[data-v-21d2b2bd] {\n  font-size: 1.1em;\n}\n.page[data-v-21d2b2bd] {\n  margin-top: 100px;\n}\n.bg-custom[data-v-21d2b2bd] {\n  background-image: linear-gradient(to right bottom, #4eb888, #37ab8a, #219e8a, #0b9187, #008383);\n  color: #f8fafc;\n}\n.card[data-v-21d2b2bd] {\n  font-size: 1.2em;\n}\n.btn-modal[data-v-21d2b2bd] {\n  margin-bottom: -30px;\n}\n.text-muted[data-v-21d2b2bd] {\n  margin: 0;\n}\n.form-control[data-v-21d2b2bd] {\n  width: 22%;\n  text-align: center;\n}\n.adresse[data-v-21d2b2bd] {\n  font-size: 0.9em;\n}\n", ""]);
 
 // exports
 
@@ -39966,7 +40021,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Email:")]
@@ -40003,7 +40058,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Nationalité:")]
@@ -40040,7 +40095,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Tél:")]
@@ -40077,7 +40132,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Diplômes:")]
@@ -40114,7 +40169,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Job:")]
@@ -40151,7 +40206,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Bio:")]
@@ -40192,7 +40247,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Adresse:")]
@@ -40229,7 +40284,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Linkedin:")]
@@ -40266,7 +40321,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Instagram:")]
@@ -40303,7 +40358,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "recipient-name" }
                           },
                           [_vm._v("Twitter:")]
@@ -40340,7 +40395,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "message-text" }
                           },
                           [_vm._v("Facebook:")]
@@ -40377,7 +40432,7 @@ var render = function() {
                         _c(
                           "label",
                           {
-                            staticClass: "col-form-label",
+                            staticClass: "col-form-label custom-label",
                             attrs: { for: "message-text" }
                           },
                           [_vm._v("Centres d'interêts:")]
@@ -40422,7 +40477,7 @@ var render = function() {
                           staticClass: "btn btn-secondary",
                           attrs: { type: "button", "data-dismiss": "modal" }
                         },
-                        [_vm._v("Annuler")]
+                        [_vm._v("Fermer")]
                       ),
                       _vm._v(" "),
                       _c(
@@ -40672,11 +40727,11 @@ var render = function() {
                         _vm._v(_vm._s(_vm.user_infos.job))
                       ]),
                       _vm._v(" "),
-                      _c("p", { staticClass: "text-muted font-size-sm" }, [
+                      _c("p", { staticClass: "text-muted adresse" }, [
                         _vm._v(_vm._s(_vm.user_infos.adresse))
                       ]),
                       _vm._v(" "),
-                      _c("p", { staticClass: "text-muted font-size-sm" }, [
+                      _c("p", { staticClass: "text-muted" }, [
                         _vm._v(
                           "code ami : " + _vm._s(_vm.user_infos.friend_code)
                         )
@@ -40685,10 +40740,164 @@ var render = function() {
                   ]
                 )
               ])
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "card mt-md-4 mt-2 shadow bg-light" }, [
+              _c("div", { staticClass: "card-body" }, [
+                _c("div", { staticClass: "row" }, [
+                  _c("div", { staticClass: "col d-inline-flex" }, [
+                    _c("div", { staticClass: "col-sm-3" }, [
+                      _c("h5", { staticClass: "mb-0" }, [
+                        _c(
+                          "svg",
+                          {
+                            staticClass: "bi bi-linkedin",
+                            attrs: {
+                              xmlns: "http://www.w3.org/2000/svg",
+                              width: "16",
+                              height: "16",
+                              fill: "currentColor",
+                              viewBox: "0 0 16 16"
+                            }
+                          },
+                          [
+                            _c("path", {
+                              attrs: {
+                                d:
+                                  "M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"
+                              }
+                            })
+                          ]
+                        )
+                      ])
+                    ]),
+                    _vm._v(" "),
+                    _c("div", { staticClass: "col-sm-9 text-secondary" }, [
+                      _vm._v(
+                        "\n                " +
+                          _vm._s(_vm.user_infos.linkedin) +
+                          "\n              "
+                      )
+                    ])
+                  ]),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "col d-inline-flex" }, [
+                    _c("div", { staticClass: "col-sm-3" }, [
+                      _c("h5", { staticClass: "mb-0" }, [
+                        _c(
+                          "svg",
+                          {
+                            staticClass: "bi bi-twitter",
+                            attrs: {
+                              xmlns: "http://www.w3.org/2000/svg",
+                              width: "16",
+                              height: "16",
+                              fill: "currentColor",
+                              viewBox: "0 0 16 16"
+                            }
+                          },
+                          [
+                            _c("path", {
+                              attrs: {
+                                d:
+                                  "M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"
+                              }
+                            })
+                          ]
+                        )
+                      ])
+                    ]),
+                    _vm._v(" "),
+                    _c("div", { staticClass: "col-sm-9 text-secondary" }, [
+                      _vm._v(
+                        "\n                " +
+                          _vm._s(_vm.user_infos.twitter) +
+                          "\n              "
+                      )
+                    ])
+                  ])
+                ]),
+                _vm._v(" "),
+                _c("hr"),
+                _vm._v(" "),
+                _c("div", { staticClass: "row" }, [
+                  _c("div", { staticClass: "col d-inline-flex" }, [
+                    _c("div", { staticClass: "col-sm-3" }, [
+                      _c("h5", { staticClass: "mb-0" }, [
+                        _c(
+                          "svg",
+                          {
+                            staticClass: "bi bi-instagram",
+                            attrs: {
+                              xmlns: "http://www.w3.org/2000/svg",
+                              width: "16",
+                              height: "16",
+                              fill: "currentColor",
+                              viewBox: "0 0 16 16"
+                            }
+                          },
+                          [
+                            _c("path", {
+                              attrs: {
+                                d:
+                                  "M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"
+                              }
+                            })
+                          ]
+                        )
+                      ])
+                    ]),
+                    _vm._v(" "),
+                    _c("div", { staticClass: "col-sm-9 text-secondary" }, [
+                      _vm._v(
+                        "\n                " +
+                          _vm._s(_vm.user_infos.instagram) +
+                          "\n              "
+                      )
+                    ])
+                  ]),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "col d-inline-flex" }, [
+                    _c("div", { staticClass: "col-sm-3" }, [
+                      _c("h5", { staticClass: "mb-0" }, [
+                        _c(
+                          "svg",
+                          {
+                            staticClass: "bi bi-facebook",
+                            attrs: {
+                              xmlns: "http://www.w3.org/2000/svg",
+                              width: "16",
+                              height: "16",
+                              fill: "currentColor",
+                              viewBox: "0 0 16 16"
+                            }
+                          },
+                          [
+                            _c("path", {
+                              attrs: {
+                                d:
+                                  "M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"
+                              }
+                            })
+                          ]
+                        )
+                      ])
+                    ]),
+                    _vm._v(" "),
+                    _c("div", { staticClass: "col-sm-9 text-secondary" }, [
+                      _vm._v(
+                        "\n                " +
+                          _vm._s(_vm.user_infos.facebook) +
+                          "\n              "
+                      )
+                    ])
+                  ])
+                ])
+              ])
             ])
           ]),
           _vm._v(" "),
-          _c("div", { staticClass: "col-md-7" }, [
+          _c("div", { staticClass: "col-md-7 mt-md-0 mt-2" }, [
             _c("div", { staticClass: "card shadow bg-light" }, [
               _c("div", { staticClass: "card-body" }, [
                 _c("div", { staticClass: "row" }, [
@@ -40761,7 +40970,7 @@ var render = function() {
               ])
             ]),
             _vm._v(" "),
-            _c("div", { staticClass: "card mt-md-4 shadow bg-light" }, [
+            _c("div", { staticClass: "card mt-md-4 mt-2 shadow bg-light" }, [
               _c("div", { staticClass: "card-body" }, [
                 _c("form", { staticClass: "row" }, [
                   _c("div", { staticClass: "form-inline mx-sm-2 mb-2" }, [
@@ -58538,8 +58747,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuex__WEBPACK_IMPORTED_MODULE_1__
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/jubelix/test/Portreto/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/jubelix/test/Portreto/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/jubelix/Programmation/Projets/Portreto/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/jubelix/Programmation/Projets/Portreto/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
